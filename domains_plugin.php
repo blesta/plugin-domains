7c264a99881f286646f9c08ffb806b0aadd4f7a0
Jonathan Reissmueller <jonathan@phillipsdata.com>
2021-06-17 10:13:41 -0700

    Removed unnecessary model load

--- domains_plugin.php
+++ domains_plugin.php
@@ -149,7 +149,6 @@ class DomainsPlugin extends Plugin
      */
     public function upgrade($current_version, $plugin_id)
     {
-        Loader::loadModels($this, ['Actions', 'Companies', 'Navigation']);
         Configure::load('domains', dirname(__FILE__) . DS . 'config' . DS);
         if (!isset($this->Record)) {
             Loader::loadComponents($this, ['Record']);
