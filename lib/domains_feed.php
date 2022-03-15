50e907f010475d0011875ba5258aebf5908efcb6
Abdy Franco <yosoy@abdyfran.co>
2022-03-14 20:36:42 -0600

    Updated data feed to point to the correct view

--- lib/domains_feed.php
+++ lib/domains_feed.php
@@ -149,7 +149,7 @@ class DomainsFeed extends AbstractDataFeed
             }
 
             // Load the data feed view
-            $this->view = new View($vars['style'] . '_table', 'feeds');
+            $this->view = new View($vars['style'] . '_table', 'domains.feeds');
             $this->view->setDefaultView(realpath(dirname(__FILE__) . DS . '..') . DS);
 
             Loader::loadHelpers($this, ['CurrencyFormat']);
