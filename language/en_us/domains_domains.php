ade3deb3e29006b6fadb3e7ddf5af5d6e69bf2c0
Abdy Franco <abdy@devmail.one>
2024-09-05 16:03:42 -0600

    Disallow domain renewals if there is service changes pending

--- language/en_us/domains_domains.php
+++ language/en_us/domains_domains.php
@@ -2,6 +2,6 @@
 /*
  * en_us language for the Domain Manager Domains model
  */
-$lang['DomainsDomains.!error.invoices_renew_service'] = 'The domain cannot be renewed until all invoices containing this domain have been paid.';
+$lang['DomainsDomains.!error.invoices_renew_service'] = 'The domain cannot be renewed until all invoices and service changes containing this domain have been processed.';
 $lang['DomainsDomains.!error.invalid_term'] = 'This domain cannot be renewed for the selected amount of years.';
 $lang['DomainsDomains.!error.unsupported_tld'] = 'The domain TLD is unsupported by the selected registrar module.';
