3d4c1dd627ccbc042a3f938f1c256165542b0f12
Jonathan Reissmueller <jonathan@phillipsdata.com>
2025-11-14 10:28:24 -0500

    Consolidated nameserver and welcome email scope option variables

--- controllers/admin_domains.php
+++ controllers/admin_domains.php
@@ -2327,7 +2327,7 @@ class AdminDomains extends DomainsController
         }
 
         // Fetch update scopes
-        $nameserver_scope = $update_scopes = $this->getUpdateScopes();
+        $update_scopes = $this->getUpdateScopes();
 
         // Check if there are multiple packages for this TLD
         $has_multiple_packages = $this->DomainsTlds->hasMultipleTldPackages(
@@ -2342,7 +2342,6 @@ class AdminDomains extends DomainsController
                 'package',
                 'package_fields',
                 'package_fields_view',
-                'nameserver_scope',
                 'update_scopes',
                 'tld',
                 'currencies',
